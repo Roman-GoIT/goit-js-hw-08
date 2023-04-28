ac1b16cadc990764a8f5d19ab6e497364108278b
Roman-GoIT <best4you.ua@gmail.com>
2023-04-28 17:21:30 +0300

    update

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,40 +1,38 @@
 import throttle from 'lodash.throttle';
 
-const form = document.querySelector('.feedback-form');
+  const form = document.querySelector('.feedback-form');
 const key = 'feedback-form-state';
 const emailForm = document.querySelector('input[name = email]');
 const messageForm = document.querySelector('textarea[name = message]');
-const formData = {
-  email: emailForm.value,
-  message: messageForm.value,
+let formData = {
+  email: '',
+  message: '',
 };
-
 form.addEventListener('input', throttle(onInput, 500));
-
 function onInput() {
   formData.email = emailForm.value;
   formData.message = messageForm.value;
-  localStorage.setItem('key', JSON.stringify(formData));
+  localStorage.setItem(key, JSON.stringify(formData));
   if (localStorage.getItem(key)) {
     const previousData = JSON.parse(localStorage.getItem(key));
     emailForm.value = previousData.email;
     messageForm.value = previousData.message;
   }
-}
-
+};
 form.addEventListener('submit', onSubmit);
-
-function onSubmit(evt) {  
+function onSubmit(event) {
+  event.preventDefault();
   formData.email = emailForm.value;
   formData.message = messageForm.value;
-  evt.preventDefault();
-  
-  //I have changed code below
-  if (emailForm.value === '' || messageForm.value === '') {
-    return alert('Fill up all neccesary fields!');
-  }
   localStorage.removeItem(key);
   emailForm.value = '';
   messageForm.value = '';
   console.log(formData);
 }
+document.addEventListener('DOMContentLoaded', () => {
+  if (localStorage.getItem(key)) {
+    const previousData = JSON.parse(localStorage.getItem(key));
+    emailForm.value = previousData.email;
+    messageForm.value = previousData.message;
+  }
+});
